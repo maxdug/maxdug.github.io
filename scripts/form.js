6b0036325e6a6598bf5c1b3a9686fed63b8f2f95
Max Duggan <max@duggan.me>
2021-11-23 20:33:09 -0500

    updated BCG case study

--- scripts/form.js
+++ scripts/form.js
@@ -15,3 +15,16 @@ document.getElementById('wordgate-form').addEventListener('submit', function che
   && document.getElementById('bcg-input').value == 'letmein') window.location.href = '/bcg/bcg.html';
 
 });
+
+
+const imgContent = document.querySelectorAll('.img-content-hover');
+
+function showImgContent(e) {
+  for(var i = 0; i < imgContent.length; i++) {
+    x = e.pageX;
+    y = e.pageY;
+    imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
+  }
+};
+
+document.addEventListener('mousemove', showImgContent);
